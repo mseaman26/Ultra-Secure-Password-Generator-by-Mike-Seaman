a2a0fc7ae5d24e2b68c0613446ea1a688c7783fd
michael seaman <mseaman26@gmail.com>
2022-09-28 16:36:13 -0700

    did some thoughrough commenting

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 // Assignment Code
+
 //global variable declaratios
+
+//this one was provided by the starter code
 var generateBtn = document.querySelector("#generate");
 var passwordLength 
+//these next four refer to the user’s choices regarding what types of characters they want in their password
 var wantsLowerCase = false
 var wantsUpperCase = false
 var wantsNumbers = false
 var wantsSpecialChars = false
+//the variable that will eventually be the final password, initialized to an empty string
 var password = ''
-//I didn’t know how to algorithmically generate this array, so I just wrote it out
+
+//I created four arrays, one for each type of character
+
+//I didn’t know to algorithmically generate this array, so I just wrote it out
 var lowerCaseArray = ["a", "b", "c", "d", "e", "f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-//I had learned a little bit about arrow syntax from codecademy, and I used google to help me algorithmically write the capital letters array
+//I had learned a little bit about arrow syntax from codecademy, and I used google to help me algorithmically write the upper-case letters array
 var upperCaseArray = lowerCaseArray.map(element =>{
   return element.toUpperCase()
 })
@@ -20,25 +28,30 @@ var specialCharsArray = [" ","!",`"`,"#","$","%","&","'","(",")","*","+",",","-"
 var passwordArray = []              
 //function declarations
 
-//Yes I'm aware that I wrote "Validify" in the funtion title.  Oh well.  I would like to chane this to 
+// This is the function that takes the user through the prompts that will lead to a valid password length being chosen
 function getAndValidatePaswordLength(){ 
  
     passwordLength = (prompt("plase select a password length.  It must be between 8 and 128 characters long")) 
     // In order to make distinct prompts for when the user types nothing and for when the user types a non-number, 
     // I had to make a separate parsed variable.  At least, that was what worked for me
     parsedPasswordLength = parseInt(passwordLength)
+    //If the user types nothing at all
     if(!passwordLength){
       alert("You didn't type anything.  Try again. Type a length for your password that is between 8 and 128")
+      //takes the user back to the beginning of the function if their length choice is invalid
       getAndValidatePaswordLength()
+      //if the user types a non-number
     } else if(isNaN(parsedPasswordLength)){
       alert("the length must be a number. Try again, make sure it's between 8 and 128")
       getAndValidatePaswordLength()
+      //if the user types a number that is outside the desired range
     }else if (parsedPasswordLength < 8 || parsedPasswordLength > 128) {
       alert("password must be between 8 and 128 characters")
       getAndValidatePaswordLength()
     }
   
 }
+//the next four functions are simple, just setting boolean values in accordance to what categories of characters the user wants in their password
 function checkIfWantsLowerCase(){
   wantsLowerCase = confirm("Do you want your password to include lower case letters?")
 }
@@ -51,6 +64,8 @@ function checkIfWantsNumbers(){
 function checkIfWantsSpecialChars(){
   wantsSpecialChars = confirm("Do you want your password to include special characters?")
 }
+//This function is where the previous four functions are called and it makes sure that the user selected at lease one category of characters for their password
+//it will take the user back to the beginning of category selection if they failed to chose at least one
 function verifyPasswordChars(){
   checkIfWantsLowerCase()
   checkIfWantsUpperCase()
@@ -61,12 +76,15 @@ function verifyPasswordChars(){
     verifyPasswordChars()
   }
 }
+//now that we have the info we need to make a valid password, we begin!
 function generatePassword(){
   for(var i = 0; i < passwordLength; i++){
+    //"chooseChar" chooses a character from the available character categories. It is declared below
     password = password+chooseChar()
   }
   return password
 }
+//This function returns an array of arrays.  Each inner array is a list of all the available characters within a selected category. Here we use the .push() method
 function makePasswordArray() {
   var array = []
   if(wantsLowerCase){
@@ -84,9 +102,13 @@ function makePasswordArray() {
   return array
 
 }
+//this function chooses each individual character in the password.  I wanted to give equal weight to each available character category, so that the password wouldn't be dominated by the categories that have longer arrays, such as "special characters"
 function chooseChar(){
+  //declares a local variable for this particular password
   var passwordArray = makePasswordArray()
+  //This line selects an index in passwordArray, which refers to another array nested inside.  These nested arrays represent character categories. A random category is selected by generating a random number that corresponds to the indexes of passwordArray
   var charCategory = Math.floor(Math.random()*passwordArray.length);
+  // This was the hardest line of code for me to write in this project.  It takes the selected category number and uses it as an index in passwordArray.  Then another random number is generated to select a character within that nested array
   var char = passwordArray[charCategory][Math.floor(Math.random()*(passwordArray[charCategory].length))];
   return(char);
   
